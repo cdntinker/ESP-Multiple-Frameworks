d8fee2079a7f4134b9206459b7c66af0a53b24b2
cdntinker <cdntinker@gmail.com>
2023-05-14 19:36:49 -0400

    bunch-o-cleanup

--- src/esp8266-nonos-sdk/main.c
+++ src/esp8266-nonos-sdk/main.c
@@ -6,56 +6,8 @@
 
 static os_timer_t ptimer;
 
-/******************************************************************************
- * FunctionName : user_rf_cal_sector_set
- * Description  : SDK just reversed 4 sectors, used for rf init data and paramters.
- *                We add this function to force users to set rf cal sector, since
- *                we don't know which sector is free in user's application.
- *                sector map for last several sectors : ABBBCDDD
- *                A : rf cal
- *                B : at parameters
- *                C : rf init data
- *                D : sdk parameters
- * Parameters   : none
- * Returns      : rf cal sector
-*******************************************************************************/
-uint32 ICACHE_FLASH_ATTR
-user_rf_cal_sector_set(void)
-{
-    enum flash_size_map size_map = system_get_flash_size_map();
-    uint32 rf_cal_sec = 0;
 
-    switch (size_map) {
-        case FLASH_SIZE_4M_MAP_256_256:
-            rf_cal_sec = 128 - 5;
-            break;
 
-        case FLASH_SIZE_8M_MAP_512_512:
-            rf_cal_sec = 256 - 5;
-            break;
-
-        case FLASH_SIZE_16M_MAP_512_512:
-        case FLASH_SIZE_16M_MAP_1024_1024:
-            rf_cal_sec = 512 - 5;
-            break;
-
-        case FLASH_SIZE_32M_MAP_512_512:
-        case FLASH_SIZE_32M_MAP_1024_1024:
-            rf_cal_sec = 1024 - 5;
-            break;
-
-        case FLASH_SIZE_64M_MAP_1024_1024:
-            rf_cal_sec = 2048 - 5;
-            break;
-        case FLASH_SIZE_128M_MAP_1024_1024:
-            rf_cal_sec = 4096 - 5;
-            break;
-        default:
-            rf_cal_sec = 0;
-            break;
-    }
-    return rf_cal_sec;
-}
 
 void blinky(void *arg)
 {
@@ -73,8 +25,13 @@ void ICACHE_FLASH_ATTR user_init(void)
 {
     gpio_init();
 
-    uart_init(115200, 115200);
+    uart_init(74880, 74880);
+    os_printf("3\n");
+    os_printf("2\n");
+    os_printf("1\n");
+    os_printf("0\n");
     os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("This is \"esp8266-nonos-sdk\"\n");
 
     // Disable WiFi
     	wifi_set_opmode(NULL_MODE);
@@ -83,5 +40,5 @@ void ICACHE_FLASH_ATTR user_init(void)
 
     os_timer_disarm(&ptimer);
     os_timer_setfn(&ptimer, (os_timer_func_t *)blinky, NULL);
-    os_timer_arm(&ptimer, 500, 1);
+    os_timer_arm(&ptimer, 100, 1);
 }
